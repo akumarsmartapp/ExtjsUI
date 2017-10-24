a29ba7918dc7b3888bcfc1f183b0bd4ca3ad2a98
akumarsmartapp <33062974+akumarsmartapp@users.noreply.github.com>
2017-10-24 20:15:39 +0530

    demo

---
+++ ExtJsDemo/ExtJSUI/app/app.js
@@ -0,0 +1,13 @@
+﻿Ext.Loader.setConfig({ enabled: true });
+
+Ext.application({
+    name: 'EmployeeApp',
+    path: 'app',
+    autoCreateViewport: true,
+    controllers: ['EmployeeController'],
+    launch: function () {
+
+        //Manage Application
+
+    }
+});
