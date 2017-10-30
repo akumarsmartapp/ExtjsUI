c2603d53316bce8c0017a4d6c823ee49710ba0af
akumarsmartapp <33062974+akumarsmartapp@users.noreply.github.com>
2017-10-30 19:27:54 +0530

    Model added

---
+++ ExtJsDemo/ExtJSUI/Scripts/app/Model/Employee.js
@@ -0,0 +1,26 @@
+﻿Ext.define('ExtJSUI.Model.Employee', {
+    extend: 'Ext.data.Model',
+    fields: [{
+        name: 'empId',
+        type: 'int',
+        useNull: true
+    }, 'empName', 'department', 'empAddress'],
+    validations: [{
+        type: 'length',
+        field: 'email',
+        min: 1
+    }, {
+        type: 'length',
+        field: 'first',
+        min: 1
+    }, {
+        type: 'length',
+        field: 'last',
+        min: 1
+    },
+    {
+        name: 'empAge',
+        type: 'int',
+        useNull: false
+    }]
+});
