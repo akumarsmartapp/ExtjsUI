6ebf8c00995b5fcb465f9171ca4b4c98afd5a157
akumarsmartapp <33062974+akumarsmartapp@users.noreply.github.com>
2017-10-30 19:31:07 +0530

    code changes

---
+++ ExtJsDemo/ExtJSUI/Scripts/app/controller/EmployeeController.js
@@ -0,0 +1,11 @@
+﻿Ext.define('controller.EmployeeController',
+{
+    extend: 'Ext.app.Controller',
+    models: ['ExtJSUI.store.Employees'],
+    stores: ['ExtJSUI.store.Employee'],
+    views: ['ExtJSUI.view.EmployeeDetails'],
+    refs: [{
+        ref: 'employeeDetails',
+        selector: 'viewport EmployeeDetails'
+    }]
+});
