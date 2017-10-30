718bfafd40761b2bb2b54fbd9dc010467e6e13ad
akumarsmartapp <33062974+akumarsmartapp@users.noreply.github.com>
2017-10-30 19:29:14 +0530

    modified

---
+++ ExtJsDemo/ExtJSUI/Scripts/app/store/Employees.js
@@ -0,0 +1,50 @@
+﻿Ext.define('ExtJSUI.store.Employees',
+{
+    extend: 'Ext.data.Store',
+    fields: ['empId', 'empName', 'empAge', 'department', 'empAddress'],
+    storeId: 'Employees',
+    //autoLoad: true,
+    //proxy: {
+    //    type: 'api',
+    //    async: true,
+    //    id: 'my-contacts'
+	//		, url: 'http://localhost/WebApiEmployees/api/Employee/FindEmployee'
+
+	//		, reader: {
+	//		    type: 'json'
+    //, root: 'items',
+    
+	//		}
+    //}
+    proxy:
+    {
+        type: 'ajax',
+        reader:
+        {
+            root: 'data',
+            type: 'json'
+        },
+        api:
+        {
+            read: 'http://localhost/WebApiEmployees/api/Employee/FindEmployee',
+            create: 'http://localhost/WebApiEmployees/api/Employee/AddEmployeeDetails',
+            update: 'http://localhost/WebApiEmployees/api/Employee/UpdateEmployee',
+            destroy: 'http://localhost/WebApiEmployees/api/Employee/DeleteEmployee/'
+        },
+        actionMethods:
+        {
+            destroy: 'DELETE',
+            read: 'GET',
+            create: 'POST',
+            update: 'PUT'
+        }
+
+    }
+});
+
+
+
+
+
+
+
